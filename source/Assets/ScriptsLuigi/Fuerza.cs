44e148960237e7b15e8fd7ca69c06e8a28b37b16
Luigi1291 <luis-1291@hotmail.com>
2014-04-18 21:33:42 -0600

    Guia Inicio Luigi

---
+++ source/Assets/ScriptsLuigi/Fuerza.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fuerza : MonoBehaviour {
+
+	void OnMouseDown(){
+		transform.position=new Vector3(transform.position.x,transform.position.y+2,transform.position.z);
+	}
+}
---
+++ source/Assets/ScriptsLuigi/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float velocidad = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("w")){
+				transform.position=new Vector3(transform.position.x,transform.position.y,transform.position.z+velocidad);
+			}
+		if(Input.GetKey("a")){
+				transform.position=new Vector3(transform.position.x-velocidad,transform.position.y,transform.position.z);
+			}
+		if(Input.GetKey("s")){
+				transform.position=new Vector3(transform.position.x,transform.position.y,transform.position.z-velocidad);
+			}
+		if(Input.GetKey("d")){
+				transform.position=new Vector3(transform.position.x+velocidad,transform.position.y,transform.position.z);
+			}
+		}
+}
