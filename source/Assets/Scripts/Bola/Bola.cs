dfe548f82de810329f2ad04215b9461fe2547a4c
NamelessGuy <nitemare.andree@gmail.com>
2014-04-20 17:04:52 -0600

    Segundo avance
    JS bolita
    JS controlador

---
+++ source/Assets/Scripts/Bola/Bola.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bola : MonoBehaviour {
+	private bool pause = false;
+	private int max =0;
+	private int min =0;
+	private float aceleracion = 0;
+	private int time =1;
+	public int arriba =5;
+	public int lateral =1;
+
+	public int tempX;
+	public int tempY;
+	public int tempZ;
+	
+	void Start () {
+	}
+	
+
+	void setValues(int max1,int min1) {
+		max = max1;
+		min = min1;
+	}
+	
+	public void increaseTime(){
+		++time;
+		this.constantForce.force.Set(0,-1*time,0);
+	}
+	
+	void Update () {
+		if(pause==false){
+			if((this.transform.position.y >=min)||( this.transform.position.y < max)){
+				//Perder
+			}
+			if(Input.GetKey("w")){//PD: evitar que salte en el aire....
+				rigidbody.velocity.Set(rigidbody.velocity.x, rigidbody.velocity.y + (arriba*time),rigidbody.velocity.z);
+				Debug.Log("FUNCIONA");
+			}
+			if(Input.GetKey("a")){
+			//	this.rigidbody.velocity.x+=lateral*time;
+			}
+			if(Input.GetKey("d")){
+			//	this.rigidbody.velocity.x-=lateral*time;
+			}
+		}
+	}
+	
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag == "spike"){
+			//PERDER
+		}
+	}
+
+	public static Bola M() {
+		return FindObjectOfType(typeof(Bola)) as Bola;
+	}	
+}
---
+++ source/Assets/Scripts/Controlador/Controlador.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controlador : MonoBehaviour {
+
+	private float speed = 1f;
+	private int time = 0;
+	
+	public int alto = 100; // |
+	public int ancho = 100; // -
+	
+	private Bola bolita;
+	
+	void Start () {
+		bolita = Bola.M();
+	}
+	
+	void Update () {
+		aumentarVelocidad();
+		//Debug.Log(time);
+		//Debug.Log(Random.Range(0,10));
+	}
+	
+	void aumentarVelocidad(){
+		++time;
+		if(time==1000){
+			time=0;
+			time+=1;
+			bolita.increaseTime();
+		}
+	}
+}
