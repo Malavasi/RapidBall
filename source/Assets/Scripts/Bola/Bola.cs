859b5eeed019015fa5edfeb2aaae7d1cd3b57c01
NamelessGuy <nitemare.andree@gmail.com>
2014-04-22 22:39:16 -0600

    Tercer avance

--- source/Assets/Scripts/Bola/Bola.cs
+++ source/Assets/Scripts/Bola/Bola.cs
@@ -5,49 +5,77 @@ public class Bola : MonoBehaviour {
 	private bool pause = false;
 	private int max =0;
 	private int min =0;
-	private float aceleracion = 0;
+	private int der =15;
+	private int izq =-15;
 	private int time =1;
-	public int arriba =5;
-	public int lateral =1;
-
-	public int tempX;
-	public int tempY;
-	public int tempZ;
+	private bool enTierra = false;
+	
+	//private float temp = 0;
+	private Vector3 vector;
+	private Vector3 vecTemp;
+	
 	
 	void Start () {
+		this.constantForce.force = new Vector3(0f,-9.8f,0f);
+
 	}
 	
 
-	void setValues(int max1,int min1) {
-		max = max1;
-		min = min1;
+	void setValues(int maxAlto,int minAlto,int derech,int izquie) {
+		max = maxAlto;
+		min = minAlto;
+		der = derech;
+		izq = izquie;
 	}
 	
 	public void increaseTime(){
 		++time;
-		this.constantForce.force.Set(0,-1*time,0);
+		//Debug.Log("nivel " + time);
+		//this.constantForce.force.Set(0,-9.8*time,0);		
 	}
 	
 	void Update () {
 		if(pause==false){
+			vector = rigidbody.velocity;
 			if((this.transform.position.y >=min)||( this.transform.position.y < max)){
 				//Perder
 			}
-			if(Input.GetKey("w")){//PD: evitar que salte en el aire....
-				rigidbody.velocity.Set(rigidbody.velocity.x, rigidbody.velocity.y + (arriba*time),rigidbody.velocity.z);
-				Debug.Log("FUNCIONA");
+			if((Input.GetKey("w"))&&(enTierra==true)){
+				vector.y = 10f;
+				rigidbody.velocity = vector;
 			}
 			if(Input.GetKey("a")){
-			//	this.rigidbody.velocity.x+=lateral*time;
+				vector.x -= 0.2f*time;
+				rigidbody.velocity = vector;
 			}
 			if(Input.GetKey("d")){
-			//	this.rigidbody.velocity.x-=lateral*time;
+				vector.x += 0.2f*time;
+				rigidbody.velocity = vector;
+			}
+			if(this.transform.position.x<izq){
+				vecTemp.y=this.transform.position.y;
+				vecTemp.x=-14f;
+				vecTemp.z=0f;
+				this.transform.position=vecTemp;
+			}			
+			if(this.transform.position.x>der){
+				vecTemp.y=this.transform.position.y;
+				vecTemp.x=14f;
+				vecTemp.z=0f;
+				this.transform.position=vecTemp;
 			}
 		}
 	}
 	
+	void OnCollisionExit(Collision col){
+		enTierra=false;
+	}
+	
 	void OnCollisionEnter(Collision col){
-		if(col.gameObject.tag == "spike"){
+		if(col.gameObject.tag == "Plataforms"){
+			enTierra=true;
+		}
+		if(col.gameObject.tag == "Spikes"){
 			//PERDER
 		}
 	}
--- source/Assets/Scripts/Controlador/Controlador.cs
+++ source/Assets/Scripts/Controlador/Controlador.cs
@@ -3,20 +3,38 @@ using System.Collections;
 
 public class Controlador : MonoBehaviour {
 
-	private float speed = 1f;
+
 	private int time = 0;
+	GameObject[] plataformas;
+	GameObject[] spikes;
 	
-	public int alto = 100; // |
-	public int ancho = 100; // -
+	
+	public int alto = 16; // |
+	public int ancho = 20; // -
+	public int numPlataform = 4;
+	public int numSpikes = 3;
 	
 	private Bola bolita;
+	private Vector3 vector;
 	
 	void Start () {
 		bolita = Bola.M();
+		plataformas = new GameObject[numPlataform];
+		spikes = new GameObject[numSpikes];
+		for(int x=0;x<numPlataform;++x){
+			plataformas[x] = (GameObject)Instantiate((GameObject)Resources.Load("Plataforma0"));
+		}
+		for(int y=0;y<numSpikes;++y){
+			spikes[y] = (GameObject)Instantiate((GameObject)Resources.Load("Spikes0"));
+		}
+		vector.y=0;
+		vector.x=0;
+		vector.z=0;
 	}
 	
 	void Update () {
 		aumentarVelocidad();
+		moverPlataformas();
 		//Debug.Log(time);
 		//Debug.Log(Random.Range(0,10));
 	}
@@ -27,6 +45,30 @@ public class Controlador : MonoBehaviour {
 			time=0;
 			time+=1;
 			bolita.increaseTime();
+		}
+	}
+	
+	void moverPlataformas(){
+		for(int x1=0;x1<numPlataform;++x1){	
+		Debug.Log(x1);
+			if(plataformas[x1].transform.position.y>alto){//16
+			vector.y=Random.Range((-1*ancho),ancho);
+			vector.y=Random.Range((-1*alto),(-2*alto));
+			}else{
+			vector.y=plataformas[x1].transform.position.y+time;
+			vector.x=(plataformas[x1].transform.position.x);
+			plataformas[x1].transform.Translate(vector);
+			}
+		}
+		for(int y1=0;y1<numSpikes;++y1){
+			if(plataformas[y1].transform.position.y>alto){//16
+				vector.y=Random.Range((-1*ancho),ancho);
+				vector.y=Random.Range((-1*alto),(-2*alto));
+			}else{
+				vector.y=spikes[y1].transform.position.y+time;
+				vector.x=(spikes[y1].transform.position.x);
+				spikes[y1].transform.Translate(vector);
+			}
 		}
 	}
 }
